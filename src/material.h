812df6da4d3237b9d479b772c7e17b93502f7251
Mark Schloeman <markofwisdumb@gmail.com>
2022-04-17 17:18:03 -0400

    Added glass material. Needs a better name, but it's a material that has an index of refraction and allows light to refract or reflect.

--- src/material.h
+++ src/material.h
@@ -1,13 +1,24 @@
 #ifndef MATERIAL_H
 #define MATERIAL_H
 
+#include <math.h>
+
 #include "vector3.h"
 #include "color.h"
 #include "objects.h"
 
 #include "memory.h"
 
-// TODO : maybe decouple color from material that way I don't need two materials just for a different color object
+vec3 vec3_reflect(vec3 v, vec3 n) {
+	return vec3_sub(v, vec3_mul(n, 2.0 * vec3_dot(v, n)));
+}
+
+vec3 vec3_refract(vec3 v, vec3 n, float ior_ratio) {
+	float cos_theta = vec3_dot(vec3_neg(v), n);
+	vec3 horizontal_component = vec3_mul(vec3_add(v, vec3_mul(n, cos_theta)), ior_ratio);
+	vec3 vertical_component = vec3_mul(n, -1.0 * sqrtf(1.0 - vec3_lensq(horizontal_component)));
+	return vec3_add(horizontal_component, vertical_component);
+}
 
 typedef struct {
 } lambertian;
@@ -28,18 +39,55 @@ metal metal_new(float blur) {
 	return metal;
 }
 
-vec3 vec3_reflect(vec3 v, vec3 n) {
-	return vec3_sub(v, vec3_mul(n, 2.0 * vec3_dot(v, n)));
-}
-
 vec3 ScatterMetal(metal m, vec3 v, vec3 *n) {
 	//return vec3_reflect(v, *n);
 	return vec3_add(vec3_reflect(v, *n), vec3_mul(vec3_random(), m.blur));
 }
 
+// TODO : find a better name for this. It's not just glass, but any medium light can travel _through_
+typedef struct {
+	float ior;
+} glass;
+
+glass glass_new(float ior) {
+	glass glass = {};
+	glass.ior = ior;
+	return glass;
+}
+
+// Schlick's Approximation is a formula for approximating contribution of Fresnel factor in specular reflection
+// 1.0 here is tecnically n1 but it is assumed to be air all the time.
+float schlick_approximation(float cos_incident, float ior_ratio) {
+	float R0 = (1.0 - ior_ratio) / (1.0 + ior_ratio);
+	R0 = R0 * R0;
+	return R0 + (1.0 - R0) * pow(1.0 - cos_incident, 5.0);
+}
+
+// NOTE : This assumes you are leaving / entering air with ior=1.0.
+vec3 ScatterGlass(glass g, vec3 v, vec3 *n, float n1, int hit_front) {
+	float ior_ratio = (hit_front) ? n1 / g.ior : g.ior / n1;
+	vec3 unit_dir = vec3_unit(v);
+	float cos_theta = vec3_dot(vec3_neg(unit_dir), *n);
+	if (cos_theta > 1.0) cos_theta = 1.0;
+	float sin_theta = sqrtf(1 - cos_theta * cos_theta);
+
+	// NOTE : should I do something different if it's at the critical angle?
+	vec3 scattered_dir;
+	int past_critical_angle = (sin_theta * ior_ratio) > 1.0;
+	if (past_critical_angle || schlick_approximation(cos_theta, ior_ratio) > random_float()) {
+		scattered_dir = vec3_reflect(unit_dir, *n); 
+	} else {
+		scattered_dir = vec3_refract(unit_dir, *n, ior_ratio);
+	}
+	return scattered_dir;
+	
+
+}
+
 enum MaterialID {
 	Lambertian,
 	Metal,
+	Glass,
 };
 
 // NOTE : I wanted this to be analgous to the union shape from objects.hit
@@ -47,7 +95,9 @@ enum MaterialID {
 union surface {
 	lambertian lambertian;
 	metal metal;
+	glass glass;
 };
+
 
 typedef struct {
 	union surface surface;
@@ -70,6 +120,14 @@ material make_metal(float blur) {
 	return mat;
 }
 
+material make_glass(float ior) {
+	material mat = {};
+	mat.id = Glass;
+	mat.has_color = 1;
+	mat.surface.glass = glass_new(ior);
+	return mat;
+}
+
 material *add_lambertian(memory_region *region) {
 	material mat = make_lambertian();
 	return (material *)memory_region_add(region, &mat, sizeof(material));
@@ -80,14 +138,22 @@ material *add_metal(memory_region *region, float blur) {
 	return (material *)memory_region_add(region, &mat, sizeof(material));
 }
 
+material *add_glass(memory_region *region, float ior) {
+	material mat = make_glass(ior);
+	return (material *)memory_region_add(region, &mat, sizeof(material));
+}
+
 // 1 = Scattered ray, 0 = no scatter
-int Scatter(material *mat, vec3 *n, ray *r) {
+int Scatter(material *mat, int hit_front, vec3 *n, ray *r) {
 	switch (mat->id) {
 		case Lambertian:
 			r->dir = ScatterLambertian(n);
 			return 1;
 		case Metal:
 			r->dir = ScatterMetal(mat->surface.metal, r->dir, n);
+			return 1;
+		case Glass:
+			r->dir = ScatterGlass(mat->surface.glass, r->dir, n, 1.0, hit_front);
 			return 1;
 		default:
 			return 0;
