5941a223089d85835d64ac886647ef2af4caadb3
Mark Schloeman <markofwisdumb@gmail.com>
2022-05-22 13:38:32 -0400

    updated timer.h to malloc space for the timespecs. Now need to use timer_free when done.

--- src/timer.h
+++ src/timer.h
@@ -1,26 +1,32 @@
 #ifndef TIMER_H
 #define TIMER_H
 
+#include <stdlib.h>
 #include <time.h>
 
 typedef struct {
-	struct timespec start;
-	struct timespec end;
+	struct timespec *start;
+	struct timespec *end;
 } timer;
 
 static timer timer_new() {
 	timer t = {};
-	struct timespec s = {};
-	struct timespec e = {};
+	struct timespec *s = (struct timespec *)malloc(sizeof(struct timespec));
+	struct timespec *e = (struct timespec *)malloc(sizeof(struct timespec));
 	t.start = s;
 	t.end = e;
-	timespec_get(&t.start, TIME_UTC);
-	timespec_get(&t.end, TIME_UTC);
+	timespec_get(t.start, TIME_UTC);
+	timespec_get(t.end, TIME_UTC);
 	return t;
 }
 
-#define TIMER_START(t) clock_gettime(CLOCK_MONOTONIC, &t.start)
-#define TIMER_END(t) clock_gettime(CLOCK_MONOTONIC, &t.end)
-#define TIMER_DURATION_S(t) (int)(t.end.tv_sec - t.start.tv_sec)
+static void timer_free(timer *t) {
+	free(t->start);
+	free(t->end);
+}
+
+#define TIMER_START(t) clock_gettime(CLOCK_MONOTONIC, t.start)
+#define TIMER_END(t) clock_gettime(CLOCK_MONOTONIC, t.end)
+#define TIMER_DURATION_S(t) (int)(t.end->tv_sec - t.start->tv_sec)
 
 #endif
