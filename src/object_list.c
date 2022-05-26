f60877a2c4370de16774cce39c44bd849980e3b4
Mark Schloeman <markofwisdumb@gmail.com>
2022-05-26 14:16:11 -0400

    Fixed object_list_new

--- src/object_list.c
+++ src/object_list.c
@@ -3,7 +3,7 @@
 object_list object_list_new(memory_region *region, int max_objects) {
 	object_list ol = {};
 	void *empty_pointer_array = calloc(max_objects, sizeof(object *));
-	ol.objects = (object **)memory_region_add(region, empty_pointer_array, max_objects * sizeof(object *));
+	ol.objects = empty_pointer_array;
 	ol.count = 0;
 	ol.max = max_objects;
 	if (ol.objects == NULL) {
@@ -35,5 +35,9 @@ int object_list_intersect(object_list *ol, ray *r, hit_record *hitrec, thread_co
 
 object_list *add_object_list(memory_region *region, int max_objects) {
 	object_list ol = object_list_new(region, max_objects);
+	if(ol.objects == NULL) return NULL;
+	object **to_free = ol.objects;
+	ol.objects = (object **)memory_region_add(region, ol.objects, max_objects * sizeof(object *));
+	free(to_free);
 	return (object_list *)memory_region_add(region, &ol, sizeof(object_list));
 }
